ceb6179ef07086058fd1d2d9284acc6f0c5c7c1f
ironpup <christoph.ding@gmail.com>
2019-01-22 13:25:50 -0500

    Add a preliminary router, passed to middleware.

--- server/middleware.js
+++ server/middleware.js
@@ -1,5 +1,8 @@
 const middleware = function(app, express) {
   console.log('middleware ... ')
+
+  const router = require('./router.js')(express)
+
 }
 
 module.exports = middleware;
---
+++ server/router.js
@@ -0,0 +1,13 @@
+const router = function(express) {
+  console.log('router ... ')
+  
+  const router = express.Router()
+
+
+
+
+
+  return router
+}
+
+module.exports = router;
