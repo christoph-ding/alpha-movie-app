5636e168c9f5b454f4369b6d73e3f9790f4f3d16
ironpup <christoph.ding@gmail.com>
2019-01-23 13:29:28 -0500

    Stub out endpoints for database CRUD commands.

--- server/routes/movieRouter.js
+++ server/routes/movieRouter.js
@@ -3,6 +3,10 @@ const fetch = require('node-fetch');
 const movieAPIKey = require('../keys.js').movie
 const movieAPIUrl = "http://www.omdbapi.com/?t="
 
+const parseMovieResponse = function(res) {
+
+}
+
 const lookupMovie = function(req, res) {
   console.log('heeeey this is the test endpoint for movies')
   console.log('params: ', req.params)
@@ -21,9 +25,35 @@ const lookupMovie = function(req, res) {
   // })
 }
 
+const allFavorites = function(req. res) {
+  console.log('getting all movies ...')
+  // get list of all favorites from database
+}
+
+const saveFavorite = function(req, res) {
+  console.log('saving a movie as favorite ...')
+  // save a movie to the database
+}
+
+const updateRating = function(req, res) {
+  console.log('update a movie rating ...')
+  // update a movie's rating
+}
+
+const removeFavorite = function(req, res) {
+  console.log('remove a movie from favorite list ...')
+  // remove a movie from the list of favorites
+}
+
 const movieRouter = function(express) {
   const movieRouter = express.Router();
-  movieRouter.get('/:title/', lookupMovie)
+
+  movieRouter.get('/', allFavorites)
+  movieRouter.post('/', saveFavorite)
+  movieRouter.update('/', updateRating)
+  movieRouter.delete('/'. removeFavorite)
+
+  movieRouter.get('/fetch/:title/', lookupMovie)
 
   return movieRouter
 };
